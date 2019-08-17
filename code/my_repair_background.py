cffa18e6791c28e0cbbf4af85c4a6f01f9fcd3da
lovedoubledan <2716775814@qq.com>
2019-08-17 19:58:31 +0800

    Add files via upload

---
+++ code/my_repair_background.py
@@ -0,0 +1,31 @@
+import os.path as osp
+import cv2 as cv
+import glob
+import numpy as np
+import torch
+import RRDBNet_arch as arch
+from PIL import Image
+
+
+def my_repair_background(img):
+    model_path = 'RRDB_ESRGAN_x4.pth'  # models/RRDB_ESRGAN_x4.pth OR models/RRDB_PSNR_x4.pth
+    device = torch.device('cuda')  
+    
+    model = arch.RRDBNet(3, 3, 64, 23, gc=32)
+    model.load_state_dict(torch.load(model_path), strict=True)
+    model.eval()
+    model = model.to(device)
+    
+    img = img * 1.0 / 255
+    img = torch.from_numpy(np.transpose(img[:, :, [2, 1, 0]], (2, 0, 1))).float()
+    img_LR = img.unsqueeze(0)
+    img_LR = img_LR.to(device)
+
+    with torch.no_grad():
+        output = model(img_LR).data.squeeze().float().cpu().clamp_(0, 1).numpy()
+    output = np.transpose(output[[2, 1, 0], :, :], (1, 2, 0))
+    output = (output * 255.0).round())
+    
+    return output
+  
+
